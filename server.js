16dd6b7ce66ae810203e1f7f1dc14f13c42c2cd5
Sohail khan <khansohailkhan395@gmail.com>
2026-09-13 23:11:02 +0530

    Gen ai project

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const { connect } = require("mongoose");
+const app = require("./src/app.js")
+const connectToDB = require("./src/config/database.js")
+const dns = require("dns")
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+    
+connectToDB()
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000")
+})
