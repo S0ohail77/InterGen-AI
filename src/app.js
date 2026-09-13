b9cd9c680ee9b4239e69ac1f6adee8d24b232bae
Sohail khan <khansohailkhan395@gmail.com>
2026-09-13 23:57:20 +0530

    update

--- src/app.js
+++ src/app.js
@@ -4,5 +4,10 @@ const app = express()
 
 app.use(express.json())
 
+// reqire all the routes here
+const authRouter = require("../routes/auth.routes")
+
+// using all the routes here
+app.use("/api/auth", authRouter)
 
 module.exports = app
