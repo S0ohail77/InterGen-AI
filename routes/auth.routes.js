e71b34dc7405a9bcd83babb5b5534fdc512797a8
Sohail khan <khansohailkhan395@gmail.com>
2026-09-14 00:14:46 +0530

    update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,14 @@
 const {Router} = require("express");
+const authController = require("../controllers/auth.controller")
 
 const authRouter = Router();
+
+/**
+ * @route Post /api/auth/register
+ * @description Register a new user
+ * @access Public
+*/
+authRouter.post("/register",)
 
 
 module.exports = authRouter
---
+++ routes/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+const userModel = require("../models/user.model")
+
+
+/**
+ * @name registerUserController
+ * @route Post /api/auth/register
+ * @description Register a new user, expects username, email and password in the request body
+ * @access Public
+ */
+
+async function registerUserController(req,res){}
+
+module.exports = {
+    registerUserController
+}
