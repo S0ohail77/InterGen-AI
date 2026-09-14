3b444463684cc6d2a7fdf256f3f42bd18c8e7165
Sohail khan <khansohailkhan395@gmail.com>
2026-09-14 14:04:55 +0530

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/user.model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 
 /**
@@ -8,29 +10,117 @@ const userModel = require("../models/user.model")
  * @access Public
  */
 
-async function registerUserController(req,res){
 
-    const { username , email , password} = req.body
+/**Register User Controller */
+async function registerUserController(req, res) {
 
-    if(!username || !email || !password){
-        return res.status(400).json({
-            message : "Please provide username, email and  password"
+    try {
+
+        const { username, email, password } = req.body
+
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "Please provide username, email and  password"
+            })
+        }
+
+        const isUserAlreadyExists = await userModel.findOne({
+            $or: [{ username }, { email }]
+        })
+
+        if (isUserAlreadyExists) {
+
+            /* isUserAleadyExists.username == username*/
+            return res.status(400).json({
+                message: "Account already exists with this email address or username"
+            })
+        }
+
+        const hash = await bcrypt.hash(password, 10)
+
+        const user = await userModel.create({
+            username,
+            email,
+            password : hash
+        })
+
+        const token = jwt.sign(
+            { id: user._id, username: user.username },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        )
+
+        res.cookie("token", token)
+
+        res.status(201).json({
+            message: "User registered Successfully",
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        })
+
+    } catch (err) {
+        console.log("ERROR HAPPENED:", err)
+    res.status(500).json({
+        message: "Something went wrong"
         })
     }
+}
 
-    const isUserAlreadyExists = await userModel.findOne({
-        $or : [{ username }, {email}]
-    })
 
-    if(isUserAlreadyExists){
+/**
+ * @name lloginUserController
+ * @description login a user, expects email and password in the reques body
+ * @access Public
+ */
+async function loginUserController(req, res) {
 
-        /* isUserAleadyExists.username == username*/ 
-        return res.status(400).json({
-            message: "Account already exists with this email address or username"
+    try {
+
+        const { email, password } = req.body
+
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            })
+        }
+
+        const token = jwt.sign(
+            { id: user._id, username: user.username },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }        
+        )
+
+        res.cookie("token", token)
+
+        res.status(201).json({
+            message : "User loggedIn successfully",
+            user:{
+                id: user._id,
+                username : user.username,
+                email : user.email
+            }
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Something went wrong"
         })
     }
 }
 
 module.exports = {
-    registerUserController
+    registerUserController,
+    loginUserController
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,23 @@
 const {Router} = require("express");
 const authController = require("../controllers/auth.controller")
 
+
 const authRouter = Router();
 
 /**
- * @route Post /api/auth/register
+ * @route POST /api/auth/register
  * @description Register a new user
  * @access Public
 */
 authRouter.post("/register", authController.registerUserController)
+
+/**
+ * @route POST /api/auth/register
+ * @description login user with email and password
+ * @access Public
+ */
+authRouter.post("/login", authController.loginUserController)
+
 
 
 module.exports = authRouter
